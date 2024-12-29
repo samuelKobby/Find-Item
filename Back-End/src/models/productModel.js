35032fc394b9e8f9269ad1a83554bd068ac304e1
samuelKobby <sgyasifordjour@gmall.com>
2024-12-29 15:40:03 +0000

    fix: Remove price field from Product model
    - Remove price requirement from product schema
    - Add timestamps to schema
    - Clean up model exports

--- Back-End/src/models/productModel.js
+++ Back-End/src/models/productModel.js
@@ -5,10 +5,6 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  price: {
-    type: String,
-    required: true,
-  },
   category: {
     type: String,
     required: true,
@@ -16,9 +12,9 @@ const productSchema = new mongoose.Schema({
   image: {
     type: String,
     required: true,
-  },
+  }
+}, {
+  timestamps: true
 });
 
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
